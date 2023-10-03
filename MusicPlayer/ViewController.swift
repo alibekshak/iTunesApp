c24c833a6e23f7893c61f4b2bca48e1e840642e6
alibekshak <alibekshakiro1@gmail.com>
2023-10-03 10:32:37 +0600

    first

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Apple on 03.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
